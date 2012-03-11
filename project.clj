f2edca4157424d303f20017c5b5e306c071f28cf
Leandro Moreira <leandro.rhcp99@gmail.com>
2012-03-11 16:08:09 -0300

    addin g lein-midje nicer way to call test by lein

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject first-test "1.0.0-SNAPSHOT"
             :description "test clojure while I'm reading about it"
             :dependencies [[org.clojure/clojure "1.3.0"]
-                           [midje "1.3.1"]])
+                           [midje "1.3.1"]]
+            :dev-dependencies [[lein-midje "1.0.7"]])
