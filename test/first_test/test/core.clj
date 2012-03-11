04795c26181bf99d1330dfc2f15e8c4910692b63
Leandro Moreira <leandro.rhcp99@gmail.com>
2012-03-11 17:31:12 -0300

    testing function range & ignoring .swp files

--- test/first_test/test/core.clj
+++ test/first_test/test/core.clj
@@ -6,3 +6,10 @@
          (fact 
            (+ 1 0) => 1
            (* 1 1) => 1))
+
+(deftest creating-sequences
+         (fact
+           "Ranges creates ranges, doh!"
+           (range 2) => '(0 1)
+           (range 0 3) => '(0 1 2)
+           (range 0 10 2) => '(0 2 4 6 8)))
