eaf370f4b6886380c29b3fc74686e43fe94c163b
Leandro Moreira <leandro.rhcp99@gmail.com>
2012-03-11 15:45:34 -0300

    added midje framework

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject first-test "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+            :description "FIXME: write description"
+            :dependencies [[org.clojure/clojure "1.3.0"]
+                           [midje "1.3.1"]])
