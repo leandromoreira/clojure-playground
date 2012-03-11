5131f5402a686aef2be695a1782a3b85c7fbef37
Leandro Moreira <leandro.rhcp99@gmail.com>
2012-03-11 17:43:04 -0300

    playing with creational sequence functions

--- test/first_test/test/core.clj
+++ test/first_test/test/core.clj
@@ -12,4 +12,21 @@
            "Ranges creates ranges, doh!"
            (range 2) => '(0 1)
            (range 0 3) => '(0 1 2)
-           (range 0 10 2) => '(0 2 4 6 8)))
+           (range 0 10 2) => '(0 2 4 6 8))
+         (fact
+           "Repeat something x times"
+           (repeat 3 :sol) => '(:sol :sol :sol))
+         (fact
+           "Repeat returns a lazy infite sequence"
+           (take 3 (repeat :sol)) => '(:sol :sol :sol))
+         (fact
+           "Iterate a function over a value"
+           (take 2 (iterate inc 1)) => '(1 2))
+         (fact
+           "Cycle also produces a infinite sequence"
+           (take 3 (repeat :sol)) => '(:sol :sol :sol))
+         (fact
+           "Interleave collections"
+           (interleave [:a :b] [1 2]) => '(:a 1 :b 2)
+           (interleave [:a :b] [1 2 3]) => '(:a 1 :b 2)))
+
