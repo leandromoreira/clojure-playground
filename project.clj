3204d8096541237dfe249bb4e7bf1f577588e8e5
Leandro Moreira <leandro.rhcp99@gmail.com>
2012-03-11 15:49:07 -0300

    adding text for default description

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject first-test "1.0.0-SNAPSHOT"
-            :description "FIXME: write description"
+            :description "test clojure while I'm reading about it"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [midje "1.3.1"]])
